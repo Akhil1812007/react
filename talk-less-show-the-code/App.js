9db9212ebfbde26308c3b98b36eaf9cc28db7ac9
akhil <akhil.bindhu@outlook.com>
2024-09-14 09:04:52 +0530

    basic-wire-frame-swiggy-clone

--- talk-less-show-the-code/App.js
+++ talk-less-show-the-code/App.js
@@ -10,16 +10,28 @@ const AppLayout=()=>{
     )
 }
 
-const Header =()=>{
-    return(
-        <di className="Header">
-            <div className="logo-container">
-                <img className="logo"  src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726185600&semt=ais_hybrid"></img>
-            </div>
-
-        </di>
-    )
-}
+const Header = () => {
+    return (
+      <header className="header">
+        <div className="header__logo-container">
+          <img
+            className="header__logo"
+            src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726185600&semt=ais_hybrid"
+            alt="Logo"
+          />
+        </div>
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-item">Home</li>
+            <li className="header__nav-item">About</li>
+            <li className="header__nav-item">Offers</li>
+            <li className="header__nav-item">Cart</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  };
+  
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout/>)
