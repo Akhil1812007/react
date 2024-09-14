ba0cea37e11008c4cce15b40a18a58c808397496
akhil <akhil.bindhu@outlook.com>
2024-09-14 15:27:35 +0530

    basic-wire-frame-swiggy-clone-02

--- talk-less-show-the-code/App.js
+++ talk-less-show-the-code/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import restuarants  from "./swiggy-apt-data"
 
 const AppLayout=()=>{
     return (
         <div className="app">
             <Header/>
+            <ResBody/>
 
         </div>
     )
@@ -16,7 +18,7 @@ const Header = () => {
         <div className="header__logo-container">
           <img
             className="header__logo"
-            src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726185600&semt=ais_hybrid"
+            src="https://w7.pngwing.com/pngs/882/726/png-transparent-chef-cartoon-chef-photography-cooking-fictional-character-thumbnail.png"
             alt="Logo"
           />
         </div>
@@ -31,7 +33,54 @@ const Header = () => {
       </header>
     );
   };
-  
+  const allRestuarants=restuarants;
+const ResBody=()=>{
+    //console.log(allRestuarants);
+
+    return (
+        <div className="res-body">
+            <div className="search">
+                search
+            </div>
+            <div className="res-card-list">
+               
+            {
+                allRestuarants.map(
+                    (res)=>{
+                        return(<ResCard restuarnt={res}/>);
+                    }
+                )
+            }
+           
+            </div>
+        </div>
+    );
+}
+
+const ResCard=(probs)=>{
+    console.log(probs);
+    const {restuarnt}=probs;
+    const {name,cuisines,avgRating,sla,costForTwo,cloudinaryImageId}=restuarnt.info;
+    console.log(name +"name");
+    const {res}=probs;
+    //console.log(res.name);
+    return (
+      <div className="res-card">
+         <img className="food-image"alt="biriyani" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}></img>
+         <h3>{name}</h3>
+         <h4 className="cuisine">{cuisines?.join(",")}</h4>
+         <div className="rate-time">
+            <h4>{avgRating}⭐</h4>
+            <h4>{sla?.deliveryTime} 🕰️</h4>
+            <h4>{costForTwo}</h4>
+         </div>
+         
+         
+         
+      </div>
+        
+    );
+}
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout/>)
---
+++ talk-less-show-the-code/swiggy-apt-data.js
@@ -0,0 +1,611 @@
+const restuarants= [
+  {
+    "info": {
+      "id": "245677",
+      "name": "Girikrishna",
+      "cloudinaryImageId": "azsh9sjxjdjdxkdebim0",
+      "locality": "Alummoodu",
+      "areaName": "Neyattinkara Locality",
+      "costForTwo": "₹150 for two",
+      "cuisines": [
+        "Kerala",
+        "Chinese",
+        "South Indian"
+      ],
+      "avgRating": 4.3,
+      "parentId": "20328",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "5.3K+",
+      "sla": {
+        "deliveryTime": 42,
+        "lastMileTravel": 11.1,
+        "serviceability": "SERVICEABLE",
+        "slaString": "40-45 mins",
+        "lastMileTravelString": "11.1 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-09-14 21:30:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+        
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/thiruvananthapuram/girikrishna-alummoodu-neyattinkara-locality-rest245677",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "365057",
+      "name": "Bee Cake",
+      "cloudinaryImageId": "ogarmgftyelopbjgr170",
+      "locality": "Vlangamuri",
+      "areaName": "Neyyattinkara",
+      "costForTwo": "₹200 for two",
+      "cuisines": [
+        "Desserts",
+        "Chinese",
+        "South Indian",
+        "Burgers",
+        "Ice Cream",
+        "Pizzas"
+      ],
+      "avgRating": 4.3,
+      "parentId": "42947",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "3.7K+",
+      "sla": {
+        "deliveryTime": 62,
+        "lastMileTravel": 11.5,
+        "serviceability": "SERVICEABLE",
+        "slaString": "60-65 mins",
+        "lastMileTravelString": "11.5 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-09-14 22:30:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "10% OFF",
+        "subHeader": "UPTO ₹40"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/thiruvananthapuram/bee-cake-vlangamuri-neyyattinkara-rest365057",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "475457",
+      "name": "Topi Vappa Biriyani",
+      "cloudinaryImageId": "rsah7x674wgmbpfprpd0",
+      "locality": "Veppamoodu",
+      "areaName": "Marthandam",
+      "costForTwo": "₹300 for two",
+      "cuisines": [
+        "Indian",
+        "Chinese"
+      ],
+      "avgRating": 4.3,
+      "parentId": "9693",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "376",
+      "sla": {
+        "deliveryTime": 52,
+        "lastMileTravel": 8.3,
+        "serviceability": "SERVICEABLE",
+        "slaString": "50-55 mins",
+        "lastMileTravelString": "8.3 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-09-14 16:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "₹100 OFF",
+        "subHeader": "ABOVE ₹199",
+        "discountTag": "FLAT DEAL"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/thiruvananthapuram/topi-vappa-biriyani-veppamoodu-marthandam-rest475457",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "134897",
+      "name": "Uduppi Hotel",
+      "cloudinaryImageId": "sf74l21skgjr5ftwbqjg",
+      "locality": "Alummoodu",
+      "areaName": "Neyyattinkara",
+      "costForTwo": "₹150 for two",
+      "cuisines": [
+        "South Indian"
+      ],
+      "avgRating": 4.4,
+      "veg": true,
+      "parentId": "20504",
+      "avgRatingString": "4.4",
+      "totalRatingsString": "6.0K+",
+      "sla": {
+        "deliveryTime": 38,
+        "lastMileTravel": 11.2,
+        "serviceability": "SERVICEABLE",
+        "slaString": "35-40 mins",
+        "lastMileTravelString": "11.2 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-09-14 21:30:00",
+        "opened": true
+      },
+      "badges": {
+        "imageBadges": [
+          {
+            "imageId": "v1695133679/badges/Pure_Veg111.png",
+            "description": "pureveg"
+          }
+        ]
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+        
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "pureveg",
+                  "imageId": "v1695133679/badges/Pure_Veg111.png"
+                }
+              }
+            ]
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/thiruvananthapuram/uduppi-hotel-alummoodu-neyyattinkara-rest134897",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "346670",
+      "name": "Neyyar Residency",
+      "cloudinaryImageId": "ajxfpo55nevadrhhxkml",
+      "locality": "Alummoodu",
+      "areaName": "Vazhuthacaud",
+      "costForTwo": "₹200 for two",
+      "cuisines": [
+        "South Indian",
+        "North Indian",
+        "Chinese",
+        "Tandoor",
+        "Kerala",
+        "Juices"
+      ],
+      "avgRating": 4.3,
+      "parentId": "148507",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "2.9K+",
+      "sla": {
+        "deliveryTime": 45,
+        "lastMileTravel": 11.7,
+        "serviceability": "SERVICEABLE",
+        "slaString": "45-50 mins",
+        "lastMileTravelString": "11.7 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-09-14 22:15:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "20% OFF",
+        "subHeader": "UPTO ₹50"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/thiruvananthapuram/neyyar-residency-alummoodu-vazhuthacaud-rest346670",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "532684",
+      "name": "Pizza Home",
+      "cloudinaryImageId": "yf7zrp87w8v6wckqrqzt",
+      "locality": "Melpuram Block",
+      "areaName": "Marthandam",
+      "costForTwo": "₹250 for two",
+      "cuisines": [
+        "Pizzas",
+        "Beverages"
+      ],
+      "avgRating": 4.2,
+      "parentId": "158633",
+      "avgRatingString": "4.2",
+      "totalRatingsString": "433",
+      "sla": {
+        "deliveryTime": 60,
+        "lastMileTravel": 12.2,
+        "serviceability": "SERVICEABLE",
+        "slaString": "60-65 mins",
+        "lastMileTravelString": "12.2 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-09-14 22:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "20% OFF",
+        "subHeader": "UPTO ₹50"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/thiruvananthapuram/pizza-home-melpuram-block-marthandam-rest532684",
+      "type": "WEBLINK"
+    }
+  },
+  {
+    "info": {
+      "id": "671282",
+      "name": "Kozhi.In",
+      "cloudinaryImageId": "1318d35ddb5ae134d6e20be39eb17f28",
+      "locality": "Padanthalumoodu",
+      "areaName": "Marthandam",
+      "costForTwo": "₹300 for two",
+      "cuisines": [
+        "Snacks",
+        "Burgers",
+        "Fast Food"
+      ],
+      "avgRating": 4.3,
+      "parentId": "305793",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "71",
+      "sla": {
+        "deliveryTime": 60,
+        "lastMileTravel": 8.3,
+        "serviceability": "SERVICEABLE",
+        "slaString": "55-60 mins",
+        "lastMileTravelString": "8.3 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-09-14 16:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+        
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/thiruvananthapuram/kozhi-in-padanthalumoodu-marthandam-rest671282",
+      "type": "WEBLINK"
+    }
+  }
+]
+export default restuarants;
